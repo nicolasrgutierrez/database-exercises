e7b6f92f92e817299d28d3ab962b1b18374cfc96
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-23 14:56:36 -0500

    FInally figured out select exercise

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -5,6 +5,9 @@ values ('Metallica', 'Metallica', '1991', 25.2, 'Heavy Metal'),
        ('Elton John', 'Goodbye Yellow Brick Road', '1973', 8.5, 'Rock, Pop Rock, Glam Rock'),
        ('Nirvana', 'Nevermind', '1991', 16.7, 'Grunge, Alternative Rock'),
        ('Santana', 'Supernatural', '1999', 20.5, 'Latin Rock'),
-       ('Adele', '21', '2011', 26.4, 'Pop, Soul');
-
-select * from albums;
+       ('Adele', '21', '2011', 26.4, 'Pop, Soul'),
+       ('Pink Floyd', 'The Wall', '1979', 18.7, 'Progressive Rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', '1973', 24.4, 'Progressive Rock'),
+       ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', '1967', 18.2, 'Rock'),
+       ('Michael Jackson', 'Dangerous', '1991', 17.0, 'New Jack Swing, R&B, Pop'),
+       ('Michael Jackson', 'Bad', '1987', 19.7, 'Pop, Rhythm and Blues, Funk, Rock');
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,2 +1,15 @@
 use codeup_test_db;
-Select ''
+describe albums;
+select * from albums;
+
+SELECT name AS `The name of all albums by Pink Floyd:` FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date AS 'The year Sgt. Pepper''s Lonley Hearts Club Band was released:' FROM albums WHERE name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT genre AS 'The genre for Nevermind:'FROM albums WHERE name = 'Nevermind';
+
+SELECT name AS 'Albums that were released in the 1990s:' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name AS 'Albums that had less than 20 million certified sales:' FROM albums WHERE sales < 20.0;
+
+SELECT name AS  'All the albums with a genre of "Rock":' FROM albums WHERE genre = 'Rock';
