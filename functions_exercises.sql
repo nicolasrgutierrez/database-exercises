203342e23210d90fcf9a459d0162ce8c5b03bb32
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-25 12:25:57 -0500

    index exercise

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,8 +1,12 @@
 use employees;
 
-select last_name
-from employees.employees
-where last_name like 'e%' or last_name like '%e';
+# select last_name
+# from employees.employees
+# where last_name like 'e%' or last_name like '%e';
+
+select * from employees.employees e where e.first_name like'e%e';
+
+select concat(e.first_name, ' ', e.last_name) fullName from employees.employees e where e.first_name like 'e%e' and e.last_name like 'e%e';
 
 Select *
 from employees.employees
@@ -23,7 +27,7 @@ where year(hire_date) between 1990 and 1999
   order by hire_date desc;
 
 
-Select *, datediff(curdate(), hire_date)
+Select *, datediff(curdate(), hire_date) daysAthTheCompany
 from employees.employees
 where year(hire_date) between 1990 and 1999
   and month(birth_date) = 12
