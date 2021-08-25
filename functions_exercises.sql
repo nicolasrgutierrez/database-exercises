f3f6d0519f44178c3999cb45f95ee5d56ed803e7
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-25 09:39:07 -0500

    working on final problem of exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,27 @@
+use employees;
+
+select last_name
+from employees.employees
+where last_name like 'e%' or last_name like '%e';
+
+Select *
+from employees.employees
+where month(birth_date) = 12
+    and day(birth_date) = 25;
+
+Select *
+from employees.employees
+where year(hire_date) between 1990 and 1999
+  and month(birth_date) = 12
+  and day(birth_date) = 25;
+
+Select *
+from employees.employees
+where year(hire_date) between 1990 and 1999
+  and month(birth_date) = 12
+  and day(birth_date) = 25
+  order by hire_date desc;
+
+
+
+
