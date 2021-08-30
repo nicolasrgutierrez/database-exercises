564def50efda41e4181418bf275537d4fa9152ca
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2021-08-30 12:30:14 -0500

    completed bonuses

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -17,12 +17,35 @@ where t.emp_no IN (
     from employees e
     where e.first_name = 'Aamod');
 
+
 select e.first_name, e.last_name
 from employees e, dept_manager as dm
 where e.gender = 'F'
 and e.emp_no IN (
     select dm.emp_no
     where dm.to_date like '9999-01-01');
+
+
+select d.dept_name
+from employees e, dept_manager as dm, departments d
+where d.dept_no IN (
+    select dm.dept_no
+    where dm.to_date like '9999-01-01'
+    and dm.emp_no IN(
+        select e.emp_no
+        where e.gender = 'F'
+        )
+    )
+order by d.dept_name;
+
+
+select e.first_name, e.last_name
+from employees e, salaries s
+where e.emp_no IN (
+    select s.emp_no)
+order by s.salary desc
+limit 1;
+
 
 
 
